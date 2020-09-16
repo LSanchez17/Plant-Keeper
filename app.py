16ead5eb007a2b948d44471956bfc970de3914b8
LSanchez17 <galaxyplayeremail@gmail.com>
2020-09-16 18:57:19 -0400

    Heroku fixes

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "123456789")
 
 connect_db(app)
+db.create_all()
 
 ######################################################
 # Handling user instantiation into G object
